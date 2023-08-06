7f0cfb6f6472fac7e690744bcd0a0662a25dcc6c
carlacodes <carla.griffiths@gmail.com>
2023-08-06 18:26:56 +0100

    adding FRAbounds.py

---
+++ helpers/FRAbounds.py
@@ -0,0 +1,89 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def smooth_fra(z):
+    # Do some smoothing
+    s = np.array([[0.25, 0.5, 0.25],
+                  [0.5, 1, 0.5],
+                  [0.25, 0.5, 0.25]])
+    s = s / np.sum(s)  # normalize the window
+    m, n = z.shape
+    p = np.zeros((m + 2, n + 2))
+    p[1:m + 1, 1:n + 1] = z
+    p[0, :] = p[1, :]
+    p[m + 1, :] = p[m, :]
+    p[:, 0] = p[:, 1]
+    p[:, n + 1] = p[:, n]
+    z2 = np.convolve(p.flatten(), s.flatten(), 'valid').reshape((m, n))
+    return z2
+
+def FRAbounds(file, f32file):
+    fsz = 10
+    if f32file == 1:
+        r = spikerate(file, 1, 200, 1, 2)
+        r = r.T
+        ss = spikerate(file, 801, 1000, 1, 2)
+        ss = ss.T
+    else:
+        r = file
+
+    freqs = np.unique(r[:, 1])
+    levels = np.unique(r[:, 2])
+    nfreqs = len(freqs)
+    nlevels = len(levels)
+
+    spikes = np.zeros((nlevels, nfreqs))
+
+    for ff in range(nfreqs):
+        for ll in range(nlevels):
+            spikes[ll, ff] = np.mean(r[(r[:, 1] == freqs[ff]) & (r[:, 2] == levels[ll]), 0])
+
+    # Apply smoothing to the FRA
+    spikes, X, X2 = smooth_fra(spikes)
+
+    # Calculate the mean spontaneous rate
+    srate = np.mean(spikes[0, :]) + (1 / 5) * np.max(spikes)
+
+    # Determine boundaries of FRA
+    bounds = np.zeros(nfreqs)
+    for ii in range(nfreqs):
+        for jj in range(nlevels):
+            if spikes[jj, ii] < srate and jj < nlevels:
+                # no response, move on to the next level
+                pass
+            elif spikes[jj, ii] >= srate and jj == 0:
+                # response at the lowest level, move onto the next to see if real
+                pass
+            elif spikes[jj, ii] >= srate and 1 <= jj < nlevels - 2 and \
+                    spikes[jj - 1, ii] >= srate and spikes[jj + 1, ii] >= srate and spikes[jj + 2, ii] > srate:
+                bounds[ii] = jj - 1
+                break
+            elif spikes[jj, ii] >= srate and jj == nlevels - 2 and spikes[jj - 1, ii] >= srate and spikes[jj + 1, ii] >= srate:
+                bounds[ii] = jj - 1
+                break
+            elif spikes[jj, ii] >= srate and jj == nlevels - 1 and spikes[jj - 1, ii] >= srate:
+                bounds[ii] = jj - 1
+                break
+            elif spikes[jj, ii] >= srate and jj == nlevels - 1:
+                bounds[ii] = nlevels
+            elif jj == nlevels - 1 and spikes[jj, ii] < srate:
+                bounds[ii] = nlevels + 1
+
+    for ii in range(1, len(bounds) - 1):
+        if bounds[ii - 1] == nlevels + 1 and bounds[ii + 1] == nlevels + 1 and bounds[ii] < 4:
+            bounds[ii] = nlevels + 1
+
+    bf_idx = np.argwhere(bounds == np.min(bounds) - 1)
+    u = np.unique(freqs)
+    bfs = np.log(u[bf_idx])
+    bf = np.exp(np.mean(bfs))
+    data = spikes
+    return bounds, bf, srate, data, spikes
+
+# Example usage:
+# bounds, bf, Q10, Q30, Th, spikes = FRAbounds(file, f32file)
+# Here, file should be a 3 column array with mean spike rate, frequencies, and levels.
+# f32file should be set to 1 if file is in f32 format, or 0 if file is in Src format.
+# The function returns the boundaries, best frequency (bf), Q10 and Q30, threshold (Th), and smoothed spike data.
+
+# You may need to implement the spikerate function separately, as it's not provided in the code snippet you shared.
---
+++ helpers/plotfrafromspikes.py
@@ -0,0 +1,75 @@
+
+import os
+import numpy as np
+import scipy.io as sio
+import matplotlib.pyplot as plt
+from FRAbounds import FRAbounds
+
+
+
+os.chdir('D:/Data/F1815_Cruella/weekfebruary282022/F1815_Cruella')
+data = sio.loadmat('Recording_Session_Date_01-Mar-2022_Time_15-56-19.mat')
+freqs = data['currTrialDets']['Freq'][0][0].flatten()
+lvls = data['currTrialDets']['Lvl'][0][0].flatten()
+lvls = 80 - lvls
+
+os.chdir('D:/Electrophysiological Data/F1815_Cruella/HP_Block1-10')
+spikes_data = sio.loadmat('spikesBlock1-10BB2andBB3April-16-2022- 4-20-44-566-PM.mat')
+spikes = spikes_data['spikes']
+
+avgspikes = np.zeros((len(spikes), len(spikes[0])))
+for i in range(len(spikes)):
+    for i2 in range(len(spikes[i])):
+        spikesintrial = spikes[i][i2]
+        avgspikes[i][i2] = len(spikesintrial)  # / 1.2
+
+f32file = 0
+bounds, bf, Th, data, spikes = FRAbounds(avgspikes, freqs, lvls, f32file)
+
+# Plot the results
+os.makedirs('D:/Data/PSTHresults/Cruella/Block1-10', exist_ok=True)
+os.chdir('D:/Data/PSTHresults/Cruella/Block1-10')
+
+fig, axs = plt.subplots(8, 4, figsize=(20, 25), constrained_layout=True)
+
+TDTorder = [1, 3, 5, 7, 2, 4, 6, 8, 10, 12, 14, 16, 9, 11, 13, 15, 17, 19, 21, 23, 18, 20, 22, 24, 26, 28, 30, 32, 25, 27, 29, 31]
+WARPorder = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+WARPmat = np.column_stack((WARPorder, TDTorder))
+
+OrderofWARPeLCTRODESRCruella = np.array([[30, 31, 14, 15],
+                                         [28, 29, 12, 13],
+                                         [26, 27, 10, 11],
+                                         [24, 25, 8, 9],
+                                         [23, 22, 7, 6],
+                                         [21, 20, 5, 4],
+                                         [19, 18, 3, 2],
+                                         [17, 16, 1, 0]])
+
+reordermat = []
+for itest in range(32):
+    id = OrderofWARPeLCTRODESRCruella[itest, 0]
+    selectrow, selectcol = np.where(WARPmat[:, 0] == id)
+    reorder = TDTorder[selectrow]
+    reordermat.append(reorder)
+reordermat = np.array(reordermat).reshape((8, 4)).T.flatten()
+OrderofElectrodesRWARP = np.array([17, 19, 21, 23, 24, 26, 28, 30, 16, 18, 20, 22, 25, 27, 29, 31, 1, 3, 5, 7, 8, 10, 12, 14, 0, 2, 4, 6, 9, 11, 13, 15])
+OrderofElectrodesLWARP = np.array([30, 28, 26, 24, 23, 21, 19, 17, 31, 29, 27, 25, 22, 20, 18, 16, 14, 12, 10, 8, 7, 5, 3, 1, 15, 13, 11, 9, 6, 4, 2, 0])
+
+for i4 in range(32):
+    i3 = reordermat[i4]
+    i2 = np.where(WARPmat[:, 1] == i3)[0][0]
+    i22 = WARPmat[i2, 0]
+
+    axs[i4 // 4, i4 % 4].contourf(avgspikes[:, i3].reshape((-1, 1)), freqs, lvls, data[:, i3], cmap='viridis')
+    axs[i4 // 4, i4 % 4].set_title(f'TDT = {i22}, WARP = {i3}')
+    axs[i4 // 4, i4 % 4].set_xlabel('Avg Spikes')
+    axs[i4 // 4, i4 % 4].set_ylabel('Frequency (Hz)')
+
+plt.suptitle('F1815 Cruella FRA plot (March 01, 2022, Block1-10), Right Hemisphere', fontsize=16)
+plt.colorbar(axs[7, 3].images[0], ax=axs[:, -1], location='right', pad=0.01, aspect=30)
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+
+plt.savefig('FRA_F1815CruellaBlock1-10.png', dpi=300)
+plt.savefig('FRA_F1815CruellaBlock1-10.svg')
+plt.savefig('FRA_F1815CruellaBlock1-10.fig')
+plt.show()
